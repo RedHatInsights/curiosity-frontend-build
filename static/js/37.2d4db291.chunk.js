4db7f70fd8b2d46b79cbe502527ee42a529a627a
Travis CI User <travis@example.org>
2021-07-19 15:22:05 +0000

    fix(productView): ent-4108 guests toggle styling for inventory (#738)

--- static/js/37.2d4db291.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcuriosity-frontend"]=this["webpackJsonpcuriosity-frontend"]||[]).push([[37],{595:function(t,n,r){"use strict";r.r(n),r.d(n,"default",(function(){return u})),r.d(n,"InventoryTab",(function(){return u}));var e=r(0),i=r.n(e),c=r(4),u=function(t){t.active;var n=t.children;t.title;return Object(c.jsx)(i.a.Fragment,{children:n})};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=37.2d4db291.chunk.js.map
