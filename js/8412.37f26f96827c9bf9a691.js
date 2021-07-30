34aa98a79ead527395434399bff3d71222377fe1
Travis CI User <travis@example.org>
2021-07-30 21:53:08 +0000

    build(dev,proxy,prod): ent-3643 chrome local run, proxy, build (#753)
    * babel, moved to independent config from package
    * build, empty module, plugins for html, dotenv replace
    * cspell, ouia keyword added
    * docs, local run descriptions, debugging updated
    * dotenv, locale path, port, build branch
    * eslint, json extensions are now required
    * jest, adjust coverage based on fed mod file adds
    * public, index.html apply esi instead of dotenv params
    * scripts, remove original dev chrome, clean up post, proxy
    * spandx, proxy routing clean up
    * src, fed module file adds, component dev checks removed
    * tests, integration-like tests updated
    * webpack, dev, proxy, prod configs

---
+++ js/8412.37f26f96827c9bf9a691.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8412],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(92950),s=t.n(n),u=t(45237),a=t(334),o=t(94976),c=t(25097),l=t(29541),i=(t(65054),function(){return s().createElement(u.Provider,{store:c.h},s().createElement(a.BrowserRouter,{basename:o.routerHelpers.baseName},s().createElement(l.ZP,null)))})}}]);
+//# sourceMappingURL=../sourcemaps/8412.fbc88ffda2d81e1d3c4d.js.map
