3c698cd204ad0de783af03394a2644cec5929e21
Travis CI User <travis@example.org>
2021-08-09 14:07:21 +0000

    build(dev,proxy): ent-3643 activate chrome2 for local run (#757)
    * build, custom release, unused params, dev branches
    * docs, release process expanded

--- js/5020.ad728d2fcb468d4a30e5.js
+++ js/5020.ad728d2fcb468d4a30e5.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.45ce489ce2347cb8d293.js.map
+//# sourceMappingURL=../sourcemaps/5020.bad981929a37f616f67b.js.map
