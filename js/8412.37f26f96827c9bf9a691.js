04b0f72333ca65d2c00fa8055387e0820f457021
Travis CI User <travis@example.org>
2021-08-17 14:09:07 +0000

    chore(release): 2.0.0

--- js/8412.37f26f96827c9bf9a691.js
+++ js/8412.37f26f96827c9bf9a691.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8412],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(92950),s=t.n(n),u=t(45237),a=t(334),o=t(94976),c=t(25097),l=t(29541),i=(t(65054),function(){return s().createElement(u.Provider,{store:c.h},s().createElement(a.BrowserRouter,{basename:o.routerHelpers.baseName},s().createElement(l.ZP,null)))})}}]);
-//# sourceMappingURL=../sourcemaps/8412.1a0f67cbe5716096828d.js.map
+//# sourceMappingURL=../sourcemaps/8412.b6a335461e210e0ef6c5.js.map
