189556270e8747a7958fc549653fdf6ba10ee5c5
Travis CI User <travis@example.org>
2021-08-24 20:01:49 +0000

    fix(productView): ent-4247 disable take a tour header button (#774)

--- js/8412.37f26f96827c9bf9a691.js
+++ js/8412.37f26f96827c9bf9a691.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8412],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(92950),s=t.n(n),u=t(45237),a=t(334),o=t(94976),c=t(25097),l=t(29541),i=(t(65054),function(){return s().createElement(u.Provider,{store:c.h},s().createElement(a.BrowserRouter,{basename:o.routerHelpers.baseName},s().createElement(l.ZP,null)))})}}]);
-//# sourceMappingURL=../sourcemaps/8412.91ba7f2534fb7b5801a4.js.map
+//# sourceMappingURL=../sourcemaps/8412.51213cec0e786c12b35c.js.map
