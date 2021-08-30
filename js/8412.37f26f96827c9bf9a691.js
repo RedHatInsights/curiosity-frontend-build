342f3ceb011ea3f975e7ab6b8205b8eabeb95164
Travis CI User <travis@example.org>
2021-08-30 15:54:04 +0000

    chore(deps): bump react-router-dom from 5.2.0 to 5.2.1 (#777)

--- js/8412.37f26f96827c9bf9a691.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8412],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(92950),s=t.n(n),u=t(45237),a=t(334),o=t(94976),c=t(25097),l=t(29541),i=(t(65054),function(){return s().createElement(u.Provider,{store:c.h},s().createElement(a.BrowserRouter,{basename:o.routerHelpers.baseName},s().createElement(l.ZP,null)))})}}]);
-//# sourceMappingURL=../sourcemaps/8412.180401531f08365cf944.js.map
