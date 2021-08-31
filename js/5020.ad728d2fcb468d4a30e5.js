b3101921acc03297568d5a658403215cf65f0474
Travis CI User <travis@example.org>
2021-08-31 17:16:40 +0000

    chore(deps-dev): bump eslint-plugin-import from 2.24.1 to 2.24.2 (#781)

--- js/5020.ad728d2fcb468d4a30e5.js
+++ js/5020.ad728d2fcb468d4a30e5.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.b736ea4421342e6abf3d.js.map
+//# sourceMappingURL=../sourcemaps/5020.0a14b3e1f712ddf7485c.js.map
