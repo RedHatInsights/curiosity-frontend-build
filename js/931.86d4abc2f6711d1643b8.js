2fe373b79a42f8f53f8817627873d89497d9266f
Travis CI User <travis@example.org>
2021-09-07 14:35:55 +0000

    chore(deps-dev): bump babel-jest from 27.0.6 to 27.1.0 (#782)

--- js/931.86d4abc2f6711d1643b8.js
+++ js/931.86d4abc2f6711d1643b8.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.f00c7ace68643a78da06.js.map
+//# sourceMappingURL=../sourcemaps/931.eec8c7d6e7f6a6282b41.js.map
