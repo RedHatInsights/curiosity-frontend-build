47dd5c0103fc2aed61700679139a93941c6c936c
Travis CI User <travis@example.org>
2021-09-07 19:40:55 +0000

    chore(deps-dev): bump eslint-plugin-react from 7.24.0 to 7.25.1 (#787)

--- js/931.86d4abc2f6711d1643b8.js
+++ js/931.86d4abc2f6711d1643b8.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.16e065af6e0ee1fe058c.js.map
+//# sourceMappingURL=../sourcemaps/931.748c0a5f23430cba70ef.js.map
