9f0224c6585a7b3276b37515992a9eae1901aa09
Travis CI User <travis@example.org>
2021-09-14 18:01:44 +0000

    fix(rhsmServices): ent-3352 api response props, mock and tests (#786)
    * inventorySubscriptions, test updates
    * productViewRHEL, OpenShiftContainer, Satellite, initial query, test
    * rhsmServices, types, redux, response constants update

--- js/931.86d4abc2f6711d1643b8.js
+++ js/931.86d4abc2f6711d1643b8.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.6e80f104ed9ee2e1614d.js.map
+//# sourceMappingURL=../sourcemaps/931.3006fa6823f09133c864.js.map
