883c55f596f3f0aeb1961081dbd7a458873c33b1
Travis CI User <travis@example.org>
2021-09-15 19:11:12 +0000

    chore(build): key-cycle (#794)

--- js/931.86d4abc2f6711d1643b8.js
+++ js/931.86d4abc2f6711d1643b8.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.2dad97bf449d139264ef.js.map
+//# sourceMappingURL=../sourcemaps/931.f747a235388b38c5436f.js.map
