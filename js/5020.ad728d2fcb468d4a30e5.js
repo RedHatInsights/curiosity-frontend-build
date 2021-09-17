afbda8fd930d57a400382786cbe554c445ed7ba3
Travis CI User <travis@example.org>
2021-09-17 13:49:33 +0000

    chore(deps-dev): bump cspell from 5.8.0 to 5.9.1 (#792)

--- js/5020.ad728d2fcb468d4a30e5.js
+++ js/5020.ad728d2fcb468d4a30e5.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.3414f0d5f0b261ca3873.js.map
+//# sourceMappingURL=../sourcemaps/5020.43c84c8eab17c1902c28.js.map
