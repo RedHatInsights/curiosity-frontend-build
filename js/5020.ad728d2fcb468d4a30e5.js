b93e37836db93c5c74752aa1cf4fce8fb9246899
Travis CI User <travis@example.org>
2021-09-21 17:28:40 +0000

    feat(inventorySubscriptions): ent-3352 activate subs table (#795)
    * dotenv, productViews, RHEL, OpenShift, activate subs
    * locales, subs table column headers

--- js/5020.ad728d2fcb468d4a30e5.js
+++ js/5020.ad728d2fcb468d4a30e5.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.4ed756bb68be0160f93b.js.map
+//# sourceMappingURL=../sourcemaps/5020.7df27ee3f0ffb8ce09d0.js.map
