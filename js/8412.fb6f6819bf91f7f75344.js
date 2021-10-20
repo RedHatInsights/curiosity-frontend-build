aaed635b2f0ea5fe2305123bec5c6285b77c82ff
Travis CI User <travis@example.org>
2021-10-20 14:12:34 +0000

    refactor(graphCard): ent-4208 activate product hooks (#817)
    * graphCard, convert to func, product hooks
    * graphCardChart, breakout filter logic, chart wrapper
    * graphCardChartLegend, convert to func, product hooks
    * graphCardChartTooltip, product hooks
    * productView, OpenShiftContainer, graphCard props
    * productViewContext, avoid dependency cycle

--- js/8412.fb6f6819bf91f7f75344.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8412],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(92950),s=t.n(n),u=t(44745),a=t(86235),o=t(94976),c=t(95423),l=t(29541),i=(t(65054),function(){return s().createElement(u.Provider,{store:c.h},s().createElement(a.BrowserRouter,{basename:o.routerHelpers.baseName},s().createElement(l.ZP,null)))})}}]);
-//# sourceMappingURL=../sourcemaps/8412.8d6eb2dd3ca3afcb6bedeed2aa4bb8cb.js.map
