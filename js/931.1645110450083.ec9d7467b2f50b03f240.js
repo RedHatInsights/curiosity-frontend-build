5eaca01d720072f3ec0401a8ded737aa2f67df73
Travis CI User <travis@example.org>
2022-02-17 15:09:30 +0000

    feat(inventoryCardHelpers): ent-4689 custom tooltip config (#884)
    * inventoryCardHelpers, restructure, allow tooltips
    * tooltip, pf tooltip wrapper, styling

---
+++ js/931.1645110450083.ec9d7467b2f50b03f240.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.c2e88db96e1a29d5814390ab0ff1305c.js.map
