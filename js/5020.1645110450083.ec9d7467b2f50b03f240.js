b9266d50b5e118e9263fed7d1b8a70a97432a927
Travis CI User <travis@example.org>
2022-02-22 01:52:07 +0000

    fix(build): npm updates, regen lock (#887)

--- js/5020.1645110450083.ec9d7467b2f50b03f240.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.aa75de0406f8a55222b5386d63ed3ec7.js.map
---
+++ js/931.1645494607308.724fb2c58019025cbe7e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.5e8f04ae6d40748b8b6782830a8b8a93.js.map
