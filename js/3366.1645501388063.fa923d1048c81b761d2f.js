bb4948769526223690bcd2caaf59cea653748e72
Travis CI User <travis@example.org>
2022-02-22 03:44:55 +0000

    fix(testing): clean up fragment wrappers (#892)

---
+++ js/3366.1645501388063.fa923d1048c81b761d2f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[3366],{63366:(e,n,r)=>{function t(e,n){if(null==e)return{};var r,t,u={},f=Object.keys(e);for(t=0;t<f.length;t++)r=f[t],n.indexOf(r)>=0||(u[r]=e[r]);return u}r.d(n,{Z:()=>t})}}]);
+//# sourceMappingURL=../sourcemaps/3366.0f81c21040e8c4727143c26fca735d02.js.map
--- js/931.1645499158948.5e5a70e444a4634e4c84.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u,InventoryTab:()=>u});var r=n(92950),a=n.n(r),u=function(e){e.active;var t=e.children;return e.title,a().createElement(a().Fragment,null,t)};u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.7823b1f9896e566cb50a2e7243b02ad1.js.map
