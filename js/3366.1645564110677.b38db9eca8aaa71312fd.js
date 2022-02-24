d891f599eba0a02838772cd35c256f08e1907841
Travis CI User <travis@example.org>
2022-02-24 16:18:56 +0000

    feat(inventoryCardSubscriptions): ent-4772 product_name sort (#896)
    * config, allow sort on products, openshift, rhel, rhosak
    * rhsmConstants, expand sort for product_name, total_capacity

--- js/3366.1645564110677.b38db9eca8aaa71312fd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[3366],{63366:(e,n,r)=>{function t(e,n){if(null==e)return{};var r,t,u={},f=Object.keys(e);for(t=0;t<f.length;t++)r=f[t],n.indexOf(r)>=0||(u[r]=e[r]);return u}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/3366.1e2b617eefe4a30a3402da7098d409c2.js.map
---
+++ js/3366.1645719415979.4f6d87418d41d3de37bf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[3366],{63366:(e,n,r)=>{function t(e,n){if(null==e)return{};var r,t,u={},f=Object.keys(e);for(t=0;t<f.length;t++)r=f[t],n.indexOf(r)>=0||(u[r]=e[r]);return u}r.d(n,{Z:()=>t})}}]);
+//# sourceMappingURL=../sourcemaps/3366.4f92590737b52437da108d1efea565ef.js.map
