b63c2b93fd8cd9e9708c6c7fb0d23fa4d7fd0a72
Travis CI User <travis@example.org>
2022-03-01 16:14:37 +0000

    fix(rhosak): ent-4615 gibibytes to binary gigabytes (#895)

---
+++ js/931.1646151161265.198bb315287feac1e836.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,i)=>{i.r(t),i.d(t,{default:()=>o,InventoryTab:()=>o}),i(92950);var r=i(45697),n=i.n(r),o=function(e){e.active;var t=e.children;return e.title,t};o.propTypes={children:n().node.isRequired,active:n().bool,title:n().node.isRequired},o.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.b9a5b40b648121ddc923f7575708b36b.js.map
