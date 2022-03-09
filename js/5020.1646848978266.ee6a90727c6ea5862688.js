493bb4fe4b04a7edc2eff9754500c0ee6ba7110d
Travis CI User <travis@example.org>
2022-03-09 18:04:45 +0000

    chore(release): 2.3.0

---
+++ js/5020.1646848978266.ee6a90727c6ea5862688.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
+//# sourceMappingURL=../sourcemaps/5020.8f89db40a8c44bdd8a6dc69e3c9c28a4.js.map
