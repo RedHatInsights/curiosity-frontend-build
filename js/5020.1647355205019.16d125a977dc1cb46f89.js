ae2931ec5aff32ae34244b954900cd860e80b6d4
Travis CI User <travis@example.org>
2022-03-17 14:12:34 +0000

    refactor(services): ent-4669 move config to serviceConfig (#909)
    * authentication, flip to platform redux action
    * platform, userActions, move authorizeUser
    * platform, userTypes, move authorize user type
    * userReducer, flip to platform redux types
    * userSelectors, passthrough for transformed responses
    * services, move config to serviceConfig
    * serviceConfig, expand cancel ability to url funcs
    * platformServices, schemas, transformers, mock calls
    * rhsmServices, testing naming for consistency
    * userServices, remove deprecated authorizeUser func

--- js/5020.1647355205019.16d125a977dc1cb46f89.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
-//# sourceMappingURL=../sourcemaps/5020.16399c6ae8fc701f61a12d9d33f2e187.js.map
