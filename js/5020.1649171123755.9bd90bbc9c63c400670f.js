656d0f8d4941ff04abdded6d2e8f64bdae0460d7
Travis CI User <travis@example.org>
2022-04-05 15:07:18 +0000

    refactor(useReactRedux): ent-4669 expand useSelectorsResponse (#916)

---
+++ js/5020.1649171123755.9bd90bbc9c63c400670f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
+//# sourceMappingURL=../sourcemaps/5020.b927800afc7bab19c81bc9997bbdbab3.js.map
