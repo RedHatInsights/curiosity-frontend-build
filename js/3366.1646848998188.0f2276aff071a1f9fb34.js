ff9ca5ec87f98a6f4c91a0cf55826b37e7a99d36
Travis CI User <travis@example.org>
2022-04-19 13:37:05 +0000

    fix(config): ent-3230 unlimited quantity, use aria-label (#924)
    * chartIcon, pass any props
    * config, rhel, openshift, rhosak use aria-label

--- js/3366.1646848998188.0f2276aff071a1f9fb34.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[3366],{63366:(e,n,r)=>{function t(e,n){if(null==e)return{};var r,t,u={},f=Object.keys(e);for(t=0;t<f.length;t++)r=f[t],n.indexOf(r)>=0||(u[r]=e[r]);return u}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/3366.3050d630e9a47e3eb330c5e752ff370f.js.map
