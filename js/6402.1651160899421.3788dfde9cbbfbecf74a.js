52f69230da131db659af5dd4342b07919fe6c59d
Travis CI User <travis@example.org>
2022-04-28 15:50:04 +0000

    refactor(bannerMessages): ent-4899 align hook components (#926)
    * bannerMessages, context, bundle api call, logic into hook
    * productView, context, move banner under product columns
    * redux, clean up unused hooks for actions, remove selectors
    * rhsmTransformers, cloudigradeHasMismatch

---
+++ js/6402.1651160899421.3788dfde9cbbfbecf74a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[6402],{28412:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i,AppEntry:()=>i});var n=t(96985),a=t.n(n),s=t(98147),u=t(34018),c=t(94976),o=t(61072),l=t(29541),i=(t(98960),function(){return a().createElement(s.Provider,{store:o.h},a().createElement(u.BrowserRouter,{basename:c.routerHelpers.dynamicBaseName()},a().createElement(l.Z,null)))})}}]);
+//# sourceMappingURL=../sourcemaps/6402.939d573b4e3eefba331d86989cd3657c.js.map
