a96011ff0f31d5aa03a7c4f2d8caa3f2a6912213
Travis CI User <travis@example.org>
2022-05-25 16:45:10 +0000

    build(dev,proxy,prod): remove unused html template (#937)
    * webpack, html template is provided by frontend config
    * build, expand clean up to dist

---
+++ js/5020.1653496988793.eeebfd233f361e0b6660.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[5020],{65020:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u,paginationHelpers:()=>u,calculateOffsetFromPage:()=>r,calculatePageFromOffset:()=>c,isLastPage:()=>n});var r=function(e,t){return t*(e-1)||0},c=function(e,t){return e/t+1||1},n=function(e,t,a){return c(e,t)===Math.ceil(a/t)},u={calculateOffsetFromPage:r,calculatePageFromOffset:c,isLastPage:n}}}]);
+//# sourceMappingURL=../sourcemaps/5020.7477d2a2cc72a065e4867794ee6c3f54.js.map
