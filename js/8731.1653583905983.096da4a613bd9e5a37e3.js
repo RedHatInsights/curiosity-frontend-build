85c548eca0fa6b5b8a8862c1de1fd047390fe26e
Travis CI User <travis@example.org>
2022-05-26 16:53:44 +0000

    fix(rhosak): ent-5022 subscription type column activated (#939)
    * locale, subscription type strings
    * config, rhosak, replace total capacity with subs type
    * rhsmServices, schemas, constants, subs types and mock

---
+++ js/8731.1653583905983.096da4a613bd9e5a37e3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[8731,2085],{84944:(e,t,r)=>{r.r(t),r.d(t,{default:()=>l,ProductViewOpenShiftDedicated:()=>l});var o=r(96985),i=r.n(o),n=r(29175),a=r(49844),c=r(65876),l=function(){return i().createElement(n.ProductView,{toolbarGraph:i().createElement(c.ToolbarFieldRangedMonthly,{position:a.SelectPosition.right}),toolbarGraphDescription:!0})};l.defaultProps={}}}]);
+//# sourceMappingURL=../sourcemaps/8731.fcdfadd9492de3e1794d149e3210e4f1.js.map
