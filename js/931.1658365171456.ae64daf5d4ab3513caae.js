7515ee4e45d932fd952cd8bacfc1faeb3f8f788c
Travis CI User <travis@example.org>
2022-07-21 02:52:20 +0000

    build(branching): sw-450 restructure, rename (#956)
    * build, rc npm script
    * CONTRIBUTING, document branching, tags, release
    * custom_release, travis, align release branching
    * dependabot, align dev branch

--- js/931.1658365171456.ae64daf5d4ab3513caae.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:(e,t,i)=>{i.r(t),i.d(t,{default:()=>o,InventoryTab:()=>o}),i(96985);var r=i(45697),n=i.n(r),o=function(e){e.active;var t=e.children;return e.title,t};o.propTypes={children:n().node.isRequired,active:n().bool,title:n().node.isRequired},o.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.4e57a278dee1cf7a28f3492deeee6fc6.js.map
