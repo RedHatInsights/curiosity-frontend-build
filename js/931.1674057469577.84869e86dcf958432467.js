a24966db7f3c81d0ec0c067db5f99e85e490ed81
Travis CI User <travis@example.org>
2023-01-18 15:59:10 +0000

    fix(locale): sw-728 graph legend, product desc (#1034)

---
+++ js/931.1674057469577.84869e86dcf958432467.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}}),t(92950);var r=t(45697),i=t.n(r),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:i().node.isRequired,active:i().bool,title:i().node.isRequired},u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.f8ed7e9fcf8753c9a0150e7a871f0db6.js.map
