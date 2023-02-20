c24f52cf4072e37ba8e677c0b14374b696202817
Travis CI User <travis@example.org>
2023-02-20 16:51:40 +0000

    chore(release): 2.9.0

--- js/931.1676415616742.c6052b6554dcbc825534.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}}),t(92950);var r=t(45697),i=t.n(r),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:i().node.isRequired,active:i().bool,title:i().node.isRequired},u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.847cb3da498d133c559bb85136bf4aaa.js.map
