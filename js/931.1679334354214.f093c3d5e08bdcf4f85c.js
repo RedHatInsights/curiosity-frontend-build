3f2493d399f306de10b87691288a8cb9152d85d4
Travis CI User <travis@example.org>
2023-03-21 12:10:39 +0000

    chore(prod): routing patch (#1086)

--- js/931.1679334354214.f093c3d5e08bdcf4f85c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}}),t(92950);var r=t(45697),i=t.n(r),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:i().node.isRequired,active:i().bool,title:i().node.isRequired},u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.0ebb761dc7ab4dd4cb1892e36b8867f4.js.map
