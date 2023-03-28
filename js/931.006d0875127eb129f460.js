3be3c6c4ce1b50ea24a94a8f58f65bd1bca230f0
Travis CI User <travis@example.org>
2023-03-28 01:10:22 +0000

    chore(deps-dev): @babel/core from 7.21.0 to 7.21.3 (#1087)

---
+++ js/931.006d0875127eb129f460.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}}),t(92950);var r=t(45697),i=t.n(r),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:i().node.isRequired,active:i().bool,title:i().node.isRequired},u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.bfb7845def53cdb59e48869a475a5e63.js.map
