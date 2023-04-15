b168a0d78c6ba2d48428d436b343954aa714a8ec
Travis CI User <travis@example.org>
2023-04-15 02:07:52 +0000

    fix(notifications): sw-1078 do not not use redux connect (#1101)

---
+++ js/931.c4b73e23cf59c00d3490.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}}),t(92950);var r=t(45697),i=t.n(r),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:i().node.isRequired,active:i().bool,title:i().node.isRequired},u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.7716de08aab0d68c01687c79154ced0b.js.map
