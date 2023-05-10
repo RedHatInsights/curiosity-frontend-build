73b28bb79bd8b6dd69536b7474f35e28b79d4ad9
Travis CI User <travis@example.org>
2023-05-10 20:44:59 +0000

    fix(locale): sw-1256, rhel, sat, openshift instances copy (#1124)

--- js/6402.7d15c94436bbcd6c84a4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcuriosity_frontend=self.webpackChunkcuriosity_frontend||[]).push([[6402],{28412:function(n,e,t){t.r(e),t.d(e,{AppEntry:function(){return f},default:function(){return f}});var r=t(92950),u=t.n(r),c=t(77760),i=t(82410),o=t(29541),f=(t(98960),function(){return u().createElement(c.Provider,{store:i.h},u().createElement(o.Z,null))})}}]);
-//# sourceMappingURL=../sourcemaps/6402.b8c93db9e0e219777f24fed10b805e79.js.map
