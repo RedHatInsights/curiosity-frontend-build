776549483e4c58ca07d7048f3f09155dcafe997b
Travis CI User <travis@example.org>
2023-06-16 02:06:36 +0000

    feat(config): sw-925 activate rosa graph (#1135)
    * config, activate rosa graph, inventory cols
    * locale, rosa strings
    * rhsmServices, mock updates

--- js/931.328921d942e2cbf3eaf8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}});var i=t(45697),r=t.n(i),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:r().node.isRequired,active:r().bool,title:r().node.isRequired},u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.e46507fe00671b1be4a9b004aa00038d.js.map
