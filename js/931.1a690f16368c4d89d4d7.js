976e6ceddb74b6ff73c49ac86ca5bb4fce4f7957
Travis CI User <travis@example.org>
2023-07-24 20:19:49 +0000

    fix(locale): sw-1471 rosa corrections (#1149)

---
+++ js/931.1a690f16368c4d89d4d7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}});var i=t(45697),r=t.n(i),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:r().node.isRequired,active:r().bool,title:r().node.isRequired},u.defaultProps={active:!1}}}]);
+//# sourceMappingURL=../sourcemaps/931.96589cc483ee39ec1c7f3e75077336be.js.map
--- js/931.e502f9242ba5fd8c402f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[931],{80931:function(e,n,t){t.r(n),t.d(n,{InventoryTab:function(){return u},default:function(){return u}});var i=t(45697),r=t.n(i),u=function(e){e.active;var n=e.children;return e.title,n};u.propTypes={children:r().node.isRequired,active:r().bool,title:r().node.isRequired},u.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.b6ad57328d14e7acd27f92e5e369ffbc.js.map
