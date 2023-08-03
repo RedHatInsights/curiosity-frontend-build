9dfd3d45dab31842863db3f5399dcc82adcda0ff
Travis CI User <travis@example.org>
2023-08-03 17:08:47 +0000

    fix(config): sw-1570 disable rosa daily totals (#1162)

---
+++ js/exposed-./RootApp.30d808a002c9d8701599.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[8178],{28412:function(n,e,t){t.r(e),t.d(e,{AppEntry:function(){return o},default:function(){return o}});var r=t(92950),u=t.n(r),s=t(77760),c=t(82410),i=t(29541),o=(t(98960),function(){return u().createElement(s.Provider,{store:c.h},u().createElement(i.Z,null))})}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.df902c341f790757d9b69c79aa20e2e8.js.map
