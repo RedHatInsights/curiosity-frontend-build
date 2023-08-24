dc00d1aa9af0530ccfc6375389ecb10b4383378c
Travis CI User <travis@example.org>
2023-08-24 15:06:13 +0000

    fix(services): sw-1605 mock update for deprecated ids (#1170)

--- js/931.02080d4bb151ae012b46.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[931],{80931:function(e,t,n){n.r(t),n.d(t,{InventoryTab:function(){return s},default:function(){return s}});var i=n(45697),r=n.n(i);const s=e=>{let{active:t,children:n,title:i}=e;return n};s.propTypes={children:r().node.isRequired,active:r().bool,title:r().node.isRequired},s.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.b4fe754265600e6b1f74fce596fc494b.js.map
