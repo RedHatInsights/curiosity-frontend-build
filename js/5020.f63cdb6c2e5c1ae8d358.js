15e473123b98478066da85bc50b477a4621257ae
Travis CI User <travis@example.org>
2023-09-19 15:07:31 +0000

    fix(table): sw-1537 tableEmpty, apply empty table (#1192)
    * docs, correct table modules
    * locale, correct table strings
    * table, empty table props
    * tableEmpty, generate actual empty table
    * tableSkeleton, ariaLabel, summary props

--- js/5020.f63cdb6c2e5c1ae8d358.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[5020],{65020:function(t,e,n){n.r(e),n.d(e,{calculateOffsetFromPage:function(){return a},calculatePageFromOffset:function(){return u},default:function(){return s},isLastPage:function(){return c},paginationHelpers:function(){return s}});const a=(t,e)=>e*(t-1)||0,u=(t,e)=>t/e+1||1,c=(t,e,n)=>u(t,e)===Math.ceil(n/e),s={calculateOffsetFromPage:a,calculatePageFromOffset:u,isLastPage:c}}}]);
-//# sourceMappingURL=../sourcemaps/5020.7fe64855430c4643f79d3d827582cd0e.js.map
