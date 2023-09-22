003b4ba8af9d072c8b986f21e0bdf6a3f33b8590
Travis CI User <travis@example.org>
2023-09-22 18:13:52 +0000

    build(release): adding npm aliases for pipline (#1197)

--- js/5020.e2eb27dfded91e50a744.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[5020],{65020:function(t,e,n){n.r(e),n.d(e,{calculateOffsetFromPage:function(){return a},calculatePageFromOffset:function(){return u},default:function(){return s},isLastPage:function(){return c},paginationHelpers:function(){return s}});const a=(t,e)=>e*(t-1)||0,u=(t,e)=>t/e+1||1,c=(t,e,n)=>u(t,e)===Math.ceil(n/e),s={calculateOffsetFromPage:a,calculatePageFromOffset:u,isLastPage:c}}}]);
-//# sourceMappingURL=../sourcemaps/5020.0f8faed9595a8766ba90de55893bc9d8.js.map
