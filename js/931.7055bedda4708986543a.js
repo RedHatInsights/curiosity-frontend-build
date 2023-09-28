f14ad6c231dd85f5ac45707f2c4cdcf32044cca6
Travis CI User <travis@example.org>
2023-09-28 20:24:54 +0000

    refactor(deps): sw-1689 pf5 updates (#1194)
    * bannerMessages, add alert variant export
    * chartIcon, add icon size export
    * form, component event callback remaps
    * graphCard, inventory, optIn, actions to prop
    * toolbar, align attribute naming
    * styling, restructure, remove unused
    * tooltip, adjust distance for new styling
    * testing, pf5 class name snapshot updates

--- js/931.7055bedda4708986543a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscriptions=self.webpackChunksubscriptions||[]).push([[931],{80931:function(e,t,n){n.r(t),n.d(t,{InventoryTab:function(){return s},default:function(){return s}});var i=n(45697),r=n.n(i);const s=e=>{let{active:t,children:n,title:i}=e;return n};s.propTypes={children:r().node.isRequired,active:r().bool,title:r().node.isRequired},s.defaultProps={active:!1}}}]);
-//# sourceMappingURL=../sourcemaps/931.0290caa4a3dd75d27149502f4da5b583.js.map
